dc74e8dd3b5d90c4bcf0f0a7877e5e347baff9f4
Guilherme Vilela Moreira <110065040+Vilemor@users.noreply.github.com>
2026-02-22 11:32:08 -0300

    ajuste log_json

--- logs_library/decorator.py
+++ logs_library/decorator.py
@@ -6,15 +6,19 @@ def log_json(nome_logger: str = "json_logger"):
     Decorator para logar entrada, saída e exceções de funções em JSON.
     """
     def decorator(func):
+        import functools
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             logger = configurar_logger(nome_logger)
-            logger.info("Chamada da função", extra={"args": args, "kwargs": kwargs})
+            logger.info("Chamada da função", extra={
+                "input_args": args,
+                "input_kwargs": kwargs
+            })
             try:
                 result = func(*args, **kwargs)
                 logger.info("Retorno da função", extra={"retorno": result})
                 return result
-            except Exception as e:
+            except Exception:
                 logger.exception("Exceção capturada")
                 raise
         return wrapper
